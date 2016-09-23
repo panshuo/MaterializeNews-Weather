9191fdd7e22112cefd6c0ac167f5b9fdd6e0ef00
tete <leandrozebro@gmail.com>
2016-09-22 19:58:56 -0700

    changed Database to PostgreSQL.

--- config.py
+++ config.py
@@ -13,9 +13,8 @@ class Config:
     WEIBO_APP_SECRET = 'cf1eb69ab2ea726b1b0542da97160c52'
     WEIBO_CALLBACK_URI = 'http://tetewechat.ngrok.cc/oauth/weibo'
     DEBUG = True
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-    #     'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
-    SQLALCHEMY_DATABASE_URI = 'mysql://peter:peter@127.0.0.1:3306/tete'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://tete:8888@127.0.0.1/tetedb'
+
     @staticmethod
     def init_app(app):
         pass
